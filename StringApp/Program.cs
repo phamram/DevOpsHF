7aae217718674143959f549c8fc9a13b1c367749
random567 <pham.tientung@gmail.com>
2024-03-06 16:14:49 +0100

    App ready.

---
+++ StringApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "This is an example sentence.";
+
+            Console.WriteLine("Elso karakter levagva minden szobol: " + RemoveFistChar(example));
+            Console.WriteLine("Utolso karakter levagva minden szobol: " + RemoveLastChar(example));
+        }
+
+        static string RemoveFistChar(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    words[i] = words[i].Substring(1);
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+        static string RemoveLastChar(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    words[i] = words[i].Substring(0, words[i].Length - 1);
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+    }
+}
